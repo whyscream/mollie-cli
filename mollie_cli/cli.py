c44fb1aa0fca0988ca55e8d75d4ea208b70fb352
Tom Hendrikx <tom@whyscream.net>
2022-11-09 06:54:34 +0100

    Implement CLI interfaces for getting and listing arbitrary data from the API.

--- mollie_cli/cli.py
+++ mollie_cli/cli.py
@@ -1,4 +1,9 @@
+import sys
+from contextlib import contextmanager
+
 import click
+
+from .client import APIClient, APIError, ClientError
 
 
 def validate_key(ctx, param, value):
@@ -11,7 +16,31 @@ def validate_key(ctx, param, value):
     )
 
 
-@click.command()
+def format_result(result):
+    # TODO use tabulate to format the data awesome
+    click.echo(result)
+
+
+@contextmanager
+def handle_client_exceptions():
+    """Context manager that turns exceptions from the API client into errors"""
+    try:
+        yield
+    except APIError as exc:
+        click.echo(
+            f"The Mollie API returned an error: {exc}",
+            err=True,
+        )
+        sys.exit(1)
+    except ClientError as exc:
+        click.echo(
+            f"Something went wrong while handling your command: {exc}",
+            err=True,
+        )
+        sys.exit(1)
+
+
+@click.group()
 @click.version_option()
 @click.option(
     "--key",
@@ -28,10 +57,52 @@ def validate_key(ctx, param, value):
     default=False,
     help="Enable testmode when using an access token",
 )
-def cli(key, testmode):
+@click.pass_context
+def cli(ctx, key, testmode):
     if key.startswith("access_") and testmode:
         click.echo("Enabling testmode")
 
+    # Setup the APIClient and save it to the context
+    ctx.ensure_object(dict)
+    ctx.obj["client"] = APIClient(key, testmode)
+
+
+@click.command()
+@click.argument("resource_id")
+@click.pass_context
+def get(ctx, resource_id):
+    """Retrieve a single item by resource ID"""
+    client = ctx.obj["client"]
+
+    with handle_client_exceptions():
+        result = client.get(resource_id)
+
+    format_result(result)
+
+
+@click.command()
+@click.option(
+    "--limit",
+    "-l",
+    type=int,
+    default=10,
+    help="Limit the number of results",
+)
+@click.argument("resource")
+@click.pass_context
+def list_(ctx, resource, limit):
+    """List items by resource name"""
+    client = ctx.obj["client"]
+
+    with handle_client_exceptions():
+        result = client.list(resource, limit)
+
+    format_result(result)
+
+
+cli.add_command(get)
+cli.add_command(list_, "list")
+
 
 def main():
-    cli(auto_envvar_prefix="MOLLIE")
+    cli(obj={}, auto_envvar_prefix="MOLLIE")
---
+++ mollie_cli/client.py
@@ -0,0 +1,101 @@
+from mollie.api.client import Client as MollieClient
+from mollie.api.error import Error as NativeMollieError
+
+
+class APIError(Exception):
+    """An error has ocurred while talking to the API."""
+
+    pass
+
+
+class ClientError(Exception):
+    """Something went wrong in our Client."""
+
+    pass
+
+
+class APIClient:
+    def __init__(self, key, testmode):
+        self._client = self.configure_client(key)
+        self._testmode = testmode
+
+    def use_testmode(self):
+        return self._uses_access_token and self._testmode
+
+    def get_params(self, **params):
+        if self.use_testmode():
+            params.update({"testmode": "true"})
+        return params
+
+    def configure_client(self, key):
+        client = MollieClient()
+        if key.startswith(("test_", "live_")):
+            client.set_api_key(key)
+            self._uses_access_token = True
+        elif key.startswith("access_"):
+            client.set_access_token(key)
+            self._uses_access_token = True
+        else:
+            raise RuntimeError("We don't support this type of key")
+
+        return client
+
+    def list(self, resource_name, limit):
+        """List resources by"""
+        map_ = self.get_supported_resources_map()
+
+        if resource_name in map_.keys():
+            # Found a direct match
+            pass
+        else:
+            # Try to match a substring
+            resources = [x for x in map_.keys() if x.startswith(resource_name)]
+            if not resources:
+                raise ClientError(f"No resource with name '{resource_name}'")
+            elif len(resources) > 1:
+                raise ClientError(
+                    "Incomplete resource name matches multiple resources: "
+                    f"{', '.join(resources)}",
+                )
+            else:
+                resource_name = resources[0]
+
+        resource = getattr(self._client, resource_name)
+        params = self.get_params(limit=limit)
+        try:
+            return resource.list(**params)
+        except NativeMollieError as exc:
+            raise APIError(exc) from exc
+
+    def get(self, resource_id):
+        map_ = self.get_supported_resources_map()
+
+        resource = None
+        for resource_name, prefix in map_.items():
+            if resource_id.startswith(prefix):
+                resource = getattr(self._client, resource_name)
+
+        if not resource:
+            raise ClientError(
+                f"Cannot find a resource for id '{resource_id}'",
+            )
+
+        params = self.get_params()
+        try:
+            return resource.get(resource_id, **params)
+        except NativeMollieError as exc:
+            raise APIError(exc) from exc
+
+    def get_supported_resources_map(self):
+        """Generate a map of resource names and prefixes."""
+        resources = {}
+        for attrname in dir(self._client):
+            attr = getattr(self._client, attrname)
+            try:
+                prefix = getattr(attr, "RESOURCE_ID_PREFIX")
+                if prefix:
+                    resources[attrname] = prefix
+            except AttributeError:
+                pass
+
+        return resources
