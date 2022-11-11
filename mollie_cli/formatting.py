7dccaf4015ed71640dd71b450ca9b5c374718203
Tom Hendrikx <tom@whyscream.net>
2022-11-12 00:35:48 +0100

    Add CSV output formatting

--- mollie_cli/formatting.py
+++ mollie_cli/formatting.py
@@ -1,4 +1,7 @@
+import csv
+import datetime
 import json
+from io import StringIO
 
 import click
 from tabulate import tabulate
@@ -51,16 +54,96 @@ RESOURCES_LIST_PROPERTIES = {
 }
 
 
+def flatten_dict(data, key_prefix=""):
+    flattened = {}
+    for key, value in data.items():
+        if key == "_links":
+            continue
+
+        final_key = f"{key_prefix}_{key}" if key_prefix else key
+
+        if type(value) in [str, int, bool] or value is None:
+            flattened[final_key] = value
+
+        elif type(value) is dict:
+            # Yay, recursion!
+            for key, value in flatten_dict(value, final_key).items():
+                flattened[key] = value
+
+    return flattened
+
+
+def csv_format_value(value):
+    """Specific formatting for some CSV data types"""
+    if value is None:
+        return value
+
+    try:
+        # Return a datetime object and let the CSV writer emit the proper format
+        return datetime.datetime.fromisoformat(value)
+    except ValueError:
+        pass
+
+    # no special handling
+    return value
+
+
 def format_list_result(result, resource_name, formatting):
     """Format a list result into presentable data"""
+    # fetch the properties we're going to display
+    try:
+        properties = RESOURCES_LIST_PROPERTIES[resource_name]
+    except KeyError:
+        properties = RESOURCES_LIST_PROPERTIES["_default_"]
+
     if formatting == FORMAT_JSON:
         click.echo(json.dumps(result, indent=4))
 
+    elif formatting == FORMAT_CSV:
+        with StringIO(newline="") as csvfile:
+            csvwriter = csv.writer(csvfile)
+
+            # Force to list so it's editable
+            headers = list(properties.values())
+            headers_to_remove = []
+
+            rows = []
+            for obj in result:
+                row = []
+                for key in properties.values():
+                    value = getattr(obj, key)
+                    if type(value) in (str, int, bool) or value is None:
+                        row.append(value)
+
+                    elif type(value) is dict and value.keys() == ["currency", "value"]:
+                        # It's an amount, let's add an exception
+                        for sub_key in value.keys():
+                            new_header = f"{key}_{sub_key}"
+                            if new_header not in headers:
+                                # Insert the new header before the original one
+                                position = headers.index(key)
+                                headers.insert(position, new_header)
+                            row.append(value[sub_key])
+                        # now remove the original header
+                        headers_to_remove.append(key)
+
+                    else:
+                        # remove this key from the headers, we can't handle this value
+                        headers_to_remove.append(key)
+                rows.append(row)
+
+            for header in set(headers_to_remove):
+                headers.remove(header)
+
+            # Now that we have the actual header, write it
+            csvwriter.writerow(headers)
+            for row in rows:
+                csvwriter.writerow([csv_format_value(f) for f in row])
+
+            click.echo(csvfile.getvalue())
+
     elif formatting == FORMAT_TABLE:
         # Get the properties that we want to display in list formatting
-        properties = RESOURCES_LIST_PROPERTIES.get(resource_name)
-        if not properties:
-            properties = RESOURCES_LIST_PROPERTIES.get("_default_")
 
         header = properties.keys()
         table = [header]
@@ -82,11 +165,27 @@ def format_get_result(result, formatting):
     if formatting == FORMAT_JSON:
         click.echo(json.dumps(result, indent=4))
 
+    elif formatting == FORMAT_CSV:
+        with StringIO(newline="") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            headers = []
+            row = []
+            for key, value in flatten_dict(result).items():
+                headers.append(key)
+                row.append(csv_format_value(value))
+
+            csvwriter.writerow(headers)
+            csvwriter.writerow(row)
+
+            click.echo(csvfile.getvalue())
+
     elif formatting == FORMAT_TABLE:
         table = [["Property", "Value"]]
+
         for key in dir(result):
             if key.startswith("_") or key.isupper():
                 continue
+
             value = getattr(result, key)
             if not isinstance(value, (str, int, bool, dict)) and value is not None:
                 continue
